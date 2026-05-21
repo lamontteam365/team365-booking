7b5c0eda1d7607b1e59b1a2950020b6157a42d07
lamontteam365 <lamont.team365@gmail.com>
2026-05-22 09:56:28 +1200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "team365-v1";
+const CACHE = "team365-v2";
 const ASSETS = ["/", "/index.html", "/manifest.json", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", e => {
